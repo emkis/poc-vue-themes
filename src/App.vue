3aeeadf2962a6a451eb5628ed936e803350cb61b
emkis <nicolasemkis@gmail.com>
2020-11-10 19:40:26 -0300

    chore: add project files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="colors concepts entities">
+    <SectionHero />
+    <SectionHero class="concepts-dark entities-dark" />
+  </div>
+</template>
+
+<script>
+import SectionHero from '@/components/SectionHero'
+// https://medium.com/welldone-software/css-theme-organization-best-practices-a8e375d92c7c
+
+export default {
+  name: 'App',
+  components: { SectionHero },
+}
+</script>
---
+++ src/components/SectionHero/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="SectionHero">
+    <h1>Everything child needs to become a fluent reader independently</h1>
+    <p>Learn more. Stress less.</p>
+    <button type="button">🎉 App Store</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionHero',
+}
+</script>
+
+<style lang="scss" scoped>
+.SectionHero {
+  padding: 100px;
+  text-align: center;
+  background: var(--main-background-color);
+
+  > h1 {
+    margin: 0 auto;
+    max-width: 700px;
+    font-size: 50px;
+    color: var(--main-brand-color);
+  }
+
+  > p {
+    font-size: 16px;
+    font-weight: 300;
+    margin: 50px 0 30px;
+    color: var(--main-text-color);
+  }
+
+  > button {
+    border: 0;
+    border-radius: 10px;
+    padding: 16px 40px;
+    font-size: 16px;
+    font-weight: bolder;
+    background: var(--main-brand-color);
+    color: var(--action-button-text-color);
+    cursor: pointer;
+  }
+}
+</style>
