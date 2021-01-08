373872b4aaadb9a181c514336499840ca626a8a5
Nicolas Jardim <nicolasemkis@gmail.com>
2021-01-07 23:11:42 -0300

    feat(theme): add app colors variables

--- src/styles/themes/colors.js
+++ src/styles/themes/colors.js
@@ -1,8 +1,10 @@
 export const colors = Object.freeze({
-  victoria: '#574497',
-  foam: '#e0f3fd',
-  mineShaft: '#333333',
   black: '#000',
-  codGray: '#1d1d1d',
   white: '#fff',
+  cinder: '#191622',
+  screamingGreen: '#50fa7b',
+  honeysuckle: '#f1fa8c',
+  perfume: '#bd93f9',
+  anakiwa: '#8be9fd',
+  mischka: '#e1e1e6',
 })
