74433342e0e208c34a61862f6c42171a6c6dd89b
Nicolas Jardim <nicolasemkis@gmail.com>
2021-01-09 22:22:47 -0300

    feat(theme): add cincer color variants

--- src/styles/constants/colors.js
+++ src/styles/constants/colors.js
@@ -2,6 +2,8 @@ export const colors = Object.freeze({
   black: '#000',
   white: '#fff',
   cinder: '#191622',
+  cinderDark: '#14111b',
+  cinderLight: '#211d2c',
   screamingGreen: '#50fa7b',
   honeysuckle: '#f1fa8c',
   perfume: '#bd93f9',
